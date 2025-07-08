c839e391797733baa869c7a31ce9096762290cde
Michael <121832548+GhostPrime1234@users.noreply.github.com>
2025-07-08 21:45:50 +1000

    Fix image paths for search engine logos and update datetime script reference

--- js/search.js
+++ js/search.js
@@ -14,37 +14,37 @@ function handleSearchEngineSelection() {
 
         switch (selectedEngine) {
             case 'https://search.brave.com/search':
-                logoSrc = 'images/email/brave_logo.webp';
+                logoSrc = 'images/services/email/brave_logo.webp';
                 placeholderText = 'Search Brave...';
                 logoAltText = 'Brave Browser Logo';
                 break;
             case 'https://www.google.com.au/search':
-                logoSrc = 'images/email/google_logo.png';
+                logoSrc = 'images/services/email/google_logo.png';
                 placeholderText = 'Search Google...';
                 logoAltText = 'Google Logo';
                 break;
             case 'https://bing.com/search':
-                logoSrc = 'images/email/bing_logo.png';
+                logoSrc = 'images/services/email/bing_logo.png';
                 placeholderText = 'Search Bing...';
                 logoAltText = 'Bing Logo'
                 break;
             case 'https://search.yahoo.com/search':
-                logoSrc = 'images/email/yahoo_logo.png';
+                logoSrc = 'images/services/email/yahoo_logo.png';
                 placeholderText = 'Search Yahoo...';
                 logoAltText = 'Yahoo Logo';
                 break;
             case 'https://duckduckgo.com':
-                logoSrc = 'images/email/duckduckgo_logo.png';
+                logoSrc = 'images/services/email/duckduckgo_logo.png';
                 placeholderText = 'Search DuckDuckGo...';
                 logoAltText = 'Duck Duck Go Logo';
                 break;
             case 'https://www.ecosia.org/search':
-                logoSrc = 'images/email/ecosia_logo.png';
+                logoSrc = 'images/services/email/ecosia_logo.png';
                 placeholderText = 'Search Ecosia...';
                 logoAltText = 'Ecosia Logo';
                 break;
             default:
-                logoSrc = 'images/email/brave_logo.webp';
+                logoSrc = 'images/services/email/brave_logo.webp';
                 placeholderText = 'Search Brave...';
                 logoAltText = 'Brave Browser Logo';
         }
